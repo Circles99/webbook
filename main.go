8cb2200f761aa546ddbb4f309155cea522cd2ddc
刘吉铭 <liujiming0208@gmail.com>
2024-04-22 23:42:22 +0800

    viper config remote

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
-import "github.com/spf13/viper"
+import (
+	"github.com/spf13/viper"
+	_ "github.com/spf13/viper/remote"
+)
 
 func main() {
 	InitViper()
 	server := InitWebServer()
 	err := server.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initViperRemote() {
+	// etcdctl --endpoints=127.0.0.1:12379 put /webook "$(dev.yaml)"
+	viper.SetConfigType("ymal")
+	err := viper.AddRemoteProvider("etcd3",
+		// 通过webook和其他使用etcd的区别出来
+		"127.0.0.1:12379", "/webool")
+	if err != nil {
+		panic(err)
+	}
+
+	err = viper.ReadRemoteConfig()
 	if err != nil {
 		panic(err)
 	}
