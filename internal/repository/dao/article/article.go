965ee1e3785e97823d395aea85582f5769ffdf2a
刘吉铭 <liujiming0208@gmail.com>
2024-05-14 23:38:54 +0800

    dao transaction

--- internal/repository/dao/article/article.go
+++ internal/repository/dao/article/article.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"errors"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"time"
 )
 
 type ArticleDAO interface {
 	Insert(ctx context.Context, art Article) (int64, error)
 	Update(ctx context.Context, art Article) error
+	Sync(ctx context.Context, art Article) (int64, error)
+	Upsert(ctx context.Context, art PublishArticle) error
 }
 
 type GORMArticleDao struct {
@@ -20,6 +23,47 @@ func NewArticleDao(db *gorm.DB) ArticleDAO {
 	return &GORMArticleDao{
 		db: db,
 	}
+}
+
+func (dao *GORMArticleDao) Upsert(ctx context.Context, art PublishArticle) error {
+	now := time.Now().UnixMilli()
+	art.Created = now
+	art.Updated = now
+	// 相当于 inset into xxx Values xxx ON DUPLICATE KEY UPDATE
+	err := dao.db.Clauses(clause.OnConflict{
+		// mysql 只关心DoUpdates
+		DoUpdates: clause.Assignments(map[string]interface{}{
+			"title":   art.Title,
+			"content": art.Content,
+			"updated": art.Updated,
+		}),
+	}).Create(&art).Error
+	return err
+}
+
+func (dao *GORMArticleDao) Sync(ctx context.Context, art Article) (int64, error) {
+
+	var (
+		id = art.Id
+	)
+	// 先操作制作表，后操作线上表
+	// gorm帮助管理了事务的声明周期
+	err := dao.db.Transaction(func(tx *gorm.DB) error {
+		var err error
+		txDao := NewArticleDao(tx)
+		if id > 0 {
+			err = txDao.Update(ctx, art)
+		} else {
+			id, err = txDao.Insert(ctx, art)
+		}
+		if err != nil {
+			return err
+		}
+		// 操作线上库，保存数据
+
+		return txDao.Upsert(ctx, PublishArticle{Article: art})
+	})
+	return id, err
 }
 
 func (dao *GORMArticleDao) Insert(ctx context.Context, art Article) (int64, error) {
